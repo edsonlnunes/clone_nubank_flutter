55e0a9da6cda024d19f674b96ecb3a46ae65a77c
Edson Martins <edsonmartins3110@gmail.com>
2021-09-28 09:19:59 -0300

    remove o banner de debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Clone Nubank',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         appBarTheme: AppBarTheme(
